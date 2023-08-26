299d971c2ca9c76a6e865a34aaa98a02ab0edaa1
ducthinh9311 <ducthinh9311@gmail.com>
2023-08-26 21:35:03 +0700

    update final

---
+++ resources/views/admin/pages/product_category/create.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layout.master')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Product Category</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">General Form</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- left column -->
+                    <div class="col-md-12">
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Add</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form role="form" method="post" action="{{ route('admin.product_category.store') }}">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name" class="form-control" id="name"
+                                            placeholder="Enter name">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-6">
+                                            <!-- select -->
+                                            <div class="form-group">
+                                                <label>Status</label>
+                                                <select class="custom-select">
+                                                    <option>---Please Select---</option>
+                                                    <option>Show</option>
+                                                    <option>Hide</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <!-- /.card-body -->
+
+                                        <div class="card-footer">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\ProductCategory;
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -54,10 +55,21 @@ require __DIR__.'/auth.php';
 // });
 
 
-Route::get('admin/product', [ProductController::class, 'index']);
 
-Route::get('admin/user', [UserController::class, 'index']);
 
 Route::get('master', function () {
     return view('client.layout.master');
+});
+
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::get('product', [ProductController::class, 'index'])->name('product.list');
+
+    Route::get('user', [UserController::class, 'index'])->name('user.list');
+
+    Route::get('product_category', [ProductCategory::class, 'index'])->name('product_category.list');
+
+    Route::get('product_category/add', [ProductCategory::class, 'add'])->name('product_category.add');
+
+    Route::post('product_category/store', [ProductCategory::class, 'store'])->name('product_category.store');
 });
